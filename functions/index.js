09eeec38e2994d16b95c9e45e1d3f63888bb1be7
Reetikgit <reetikchitragupt@gmail.com>
2020-10-27 01:33:17 +0530

    d

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,33 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 var uniqid = require('uniqid');
+var firebase = require("firebase");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyATUjzcsSQMIKlEeBQqMGTy_4zugRTPILg",
+  authDomain: "lake-of-cakes.firebaseapp.com",
+  databaseURL: "https://lake-of-cakes.firebaseio.com",
+  projectId: "lake-of-cakes",
+  storageBucket: "lake-of-cakes.appspot.com",
+  messagingSenderId: "779843608951",
+  appId: "1:779843608951:web:48c6afe1773e2b395e8172",
+  measurementId: "G-5ER0QF0FDW"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 // const storage = require('firebase-storage');
 admin.initializeApp();
 
 exports.addProductRequest = functions.https.onCall(async(data, context) => {
   console.log(data);
-  // console.log(data.category);
-  // if (!context.auth) {
-  //   throw new functions.https.HttpsError(
-  //     'unauthenticated', 
-  //     'only authenticated admin can add requests'
-  //   );
-  // const category = data.product.category;
-  // admin.firestore().collection(category).add(data.product);
-
-  const pid = uniqid();
-  data.pid = pid;
-  const doc = await admin.firestore().collection('cakes').add(data);
-  const docData = await doc.get();
-  // const docId = await doc.getId();
-
-  return [id, docData];
+  // const pid = uniqid();
+  // data.pid = pid;
+  // const doc = await admin.firestore().collection('cakes').add(data);
+  // const docData = await doc.get();
+  // return [pid, docData];
+  let dbref=firebase.database().ref('AllProducts');
+  let newFormMessage=dbref.push();
+  newFormMessage.set(data);
+  return;
 });
-// show me place where had u fetch
-
-
-
