bcdb7418bea614b60f3fd43343b9630c56344d54
Reetikgit <reetikchitragupt@gmail.com>
2020-11-11 20:54:13 +0530

    Added Auth Files

--- public/Auth/app.js
+++ public/Auth/app.js
@@ -19,17 +19,28 @@
 /**
  * @return {!Object} The FirebaseUI config.
  */
+// var firebaseConfig = {
+//   apiKey: "AIzaSyATUjzcsSQMIKlEeBQqMGTy_4zugRTPILg",
+//   authDomain: "lake-of-cakes.firebaseapp.com",
+//   databaseURL: "https://lake-of-cakes.firebaseio.com",
+//   projectId: "lake-of-cakes",
+//   storageBucket: "lake-of-cakes.appspot.com",
+//   messagingSenderId: "779843608951",
+//   appId: "1:779843608951:web:48c6afe1773e2b395e8172",
+//   measurementId: "G-5ER0QF0FDW"
+// };
+// firebase.initializeApp(firebaseConfig);
 function getUiConfig() {
   return {
     'callbacks': {
       // Called when the user has been successfully signed in.
-      'signInSuccessWithAuthResult': function(authResult, redirectUrl) {
+      'signInSuccessWithAuthResult': function (authResult, redirectUrl) {
         if (authResult.user) {
           handleSignedInUser(authResult.user);
         }
         if (authResult.additionalUserInfo) {
           document.getElementById('is-new-user').textContent =
-              authResult.additionalUserInfo.isNewUser ?
+            authResult.additionalUserInfo.isNewUser ?
               'New User' : 'Existing User';
         }
         // Do not redirect.
@@ -49,43 +60,43 @@ function getUiConfig() {
       },
       {
         provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        scopes :[
+        scopes: [
           'public_profile',
           'email',
           'user_likes',
           'user_friends'
         ]
       },
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      {
-        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        // Whether the display name should be displayed in Sign Up page.
-        requireDisplayName: true,
-        signInMethod: getEmailSignInMethod()
-      },
-      {
-        provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-        recaptchaParameters: {
-          size: getRecaptchaMode()
-        }
-      },
-      {
-        provider: 'microsoft.com',
-        loginHintKey: 'login_hint'
-      },
-      {
-        provider: 'apple.com',
-      },
-      firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+      // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      // {
+      //   provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      //   // Whether the display name should be displayed in Sign Up page.
+      //   requireDisplayName: true,
+      //   signInMethod: getEmailSignInMethod()
+      // },
+      // {
+      //   provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+      //   recaptchaParameters: {
+      //     size: getRecaptchaMode()
+      //   }
+      // },
+      // {
+      //   provider: 'microsoft.com',
+      //   loginHintKey: 'login_hint'
+      // },
+      // {
+      //   provider: 'apple.com',
+      // },
+      // firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
     ],
     // Terms of service url.
     'tosUrl': 'https://www.google.com',
     // Privacy policy url.
     'privacyPolicyUrl': 'https://www.google.com',
     'credentialHelper': CLIENT_ID && CLIENT_ID != 'YOUR_OAUTH_CLIENT_ID' ?
-        firebaseui.auth.CredentialHelper.GOOGLE_YOLO :
-        firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM
+      firebaseui.auth.CredentialHelper.GOOGLE_YOLO :
+      firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM
   };
 }
 
@@ -100,14 +111,14 @@ ui.disableAutoSignIn();
  */
 function getWidgetUrl() {
   return '/widget#recaptcha=' + getRecaptchaMode() + '&emailSignInMethod=' +
-      getEmailSignInMethod();
+    getEmailSignInMethod();
 }
 
 
 /**
  * Redirects to the FirebaseUI widget.
  */
-var signInWithRedirect = function() {
+var signInWithRedirect = function () {
   window.location.assign(getWidgetUrl());
 };
 
@@ -115,7 +126,7 @@ var signInWithRedirect = function() {
 /**
  * Open a popup with the FirebaseUI widget.
  */
-var signInWithPopup = function() {
+var signInWithPopup = function () {
   window.open(getWidgetUrl(), 'Sign In', 'width=985,height=735');
 };
 
@@ -124,7 +135,10 @@ var signInWithPopup = function() {
  * Displays the UI for a signed in user.
  * @param {!firebase.User} user
  */
-var handleSignedInUser = function(user) {
+var counter = 0;
+var handleSignedInUser = function (user) {
+
+  counter++;
   document.getElementById('user-signed-in').style.display = 'block';
   document.getElementById('user-signed-out').style.display = 'none';
   document.getElementById('name').textContent = user.displayName;
@@ -136,12 +150,20 @@ var handleSignedInUser = function(user) {
     // the image with its original resolution (using more bandwidth than needed)
     // when it is going to be presented in smaller size.
     if ((photoURL.indexOf('googleusercontent.com') != -1) ||
-        (photoURL.indexOf('ggpht.com') != -1)) {
+      (photoURL.indexOf('ggpht.com') != -1)) {
       photoURL = photoURL + '?sz=' +
-          document.getElementById('photo').clientHeight;
+        document.getElementById('photo').clientHeight;
     }
     document.getElementById('photo').src = photoURL;
     document.getElementById('photo').style.display = 'block';
+
+    if (counter == 1) {
+
+
+      counter = 0;
+    }
+
+
   } else {
     document.getElementById('photo').style.display = 'none';
   }
@@ -151,7 +173,7 @@ var handleSignedInUser = function(user) {
 /**
  * Displays the UI for a signed out user.
  */
-var handleSignedOutUser = function() {
+var handleSignedOutUser = function () {
   document.getElementById('user-signed-in').style.display = 'none';
   document.getElementById('user-signed-out').style.display = 'block';
   ui.start('#firebaseui-container', getUiConfig());
@@ -159,23 +181,77 @@ var handleSignedOutUser = function() {
 
 // Listen to change in auth state so it displays the correct UI for when
 // the user is signed in or not.
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
   document.getElementById('loading').style.display = 'none';
   document.getElementById('loaded').style.display = 'block';
-  user ? handleSignedInUser(user) : handleSignedOutUser();
+
+  const db = firebase.firestore();
+  console.log(user)
+
+  var counter = 0;
+  if (user != null) {
+
+    db.collection("Customers").onSnapshot(async (snapshots) => {
+
+      let snapshotDocs = snapshots.docs;
+      var dbref = db.collection('Customers');
+      for (let doc of snapshotDocs) {
+    
+        let docData = doc.data();
+        if (docData.Email == user.email) {
+          var setWithMerge = dbref.doc(doc.id).update({
+            UserName: user.displayName,
+            Email: user.email,
+            Image: user.photoURL,
+            UserID: user.uid,
+            
+            
+          });
+          counter++;
+          user ? handleSignedInUser(user) : handleSignedOutUser();
+          break;
+         
+         
+        }
+
+      }
+      if (counter == 0) {
+
+        var setWithMerge = dbref.add({
+          UserName: user.displayName,
+          Email: user.email,
+          Image: user.photoURL,
+          UserID: user.uid,
+          createdDtm: firebase.firestore.FieldValue.serverTimestamp(),
+          lastLoginTime: firebase.firestore.FieldValue.serverTimestamp(),
+          token:"null"
+        });
+        user ? handleSignedInUser(user) : handleSignedOutUser();
+
+      }
+
+
+    });
+
+
+  } else {
+    user ? handleSignedInUser(user) : handleSignedOutUser();
+  }
+
+
 });
 
 /**
  * Deletes the user's account.
  */
-var deleteAccount = function() {
-  firebase.auth().currentUser.delete().catch(function(error) {
+var deleteAccount = function () {
+  firebase.auth().currentUser.delete().catch(function (error) {
     if (error.code == 'auth/requires-recent-login') {
       // The user's credential is too old. She needs to sign in again.
-      firebase.auth().signOut().then(function() {
+      firebase.auth().signOut().then(function () {
         // The timeout allows the message to be displayed after the UI has
         // changed to the signed out state.
-        setTimeout(function() {
+        setTimeout(function () {
           alert('Please sign in again to delete your account.');
         }, 1);
       });
@@ -189,12 +265,12 @@ var deleteAccount = function() {
  */
 function handleConfigChange() {
   var newRecaptchaValue = document.querySelector(
-      'input[name="recaptcha"]:checked').value;
+    'input[name="recaptcha"]:checked').value;
   var newEmailSignInMethodValue = document.querySelector(
-      'input[name="emailSignInMethod"]:checked').value;
+    'input[name="emailSignInMethod"]:checked').value;
   location.replace(
-      location.pathname + '#recaptcha=' + newRecaptchaValue +
-      '&emailSignInMethod=' + newEmailSignInMethodValue);
+    location.pathname + '#recaptcha=' + newRecaptchaValue +
+    '&emailSignInMethod=' + newEmailSignInMethodValue);
 
   // Reset the inline widget so the config changes are reflected.
   ui.reset();
@@ -205,36 +281,36 @@ function handleConfigChange() {
 /**
  * Initializes the app.
  */
-var initApp = function() {
-  document.getElementById('sign-in-with-redirect').addEventListener(
-      'click', signInWithRedirect);
-  document.getElementById('sign-in-with-popup').addEventListener(
-      'click', signInWithPopup);
-  document.getElementById('sign-out').addEventListener('click', function() {
+var initApp = function () {
+  // document.getElementById('sign-in-with-redirect').addEventListener(
+  //   'click', signInWithRedirect);
+  // document.getElementById('sign-in-with-popup').addEventListener(
+  //   'click', signInWithPopup);
+  document.getElementById('sign-out').addEventListener('click', function () {
     firebase.auth().signOut();
   });
   document.getElementById('delete-account').addEventListener(
-      'click', function() {
-        deleteAccount();
-      });
+    'click', function () {
+      deleteAccount();
+    });
 
-  document.getElementById('recaptcha-normal').addEventListener(
-      'change', handleConfigChange);
-  document.getElementById('recaptcha-invisible').addEventListener(
-      'change', handleConfigChange);
+  // document.getElementById('recaptcha-normal').addEventListener(
+  //   'change', handleConfigChange);
+  // document.getElementById('recaptcha-invisible').addEventListener(
+  //   'change', handleConfigChange);
   // Check the selected reCAPTCHA mode.
   document.querySelector(
-      'input[name="recaptcha"][value="' + getRecaptchaMode() + '"]')
-      .checked = true;
+    'input[name="recaptcha"][value="' + getRecaptchaMode() + '"]')
+    .checked = true;
 
   document.getElementById('email-signInMethod-password').addEventListener(
-      'change', handleConfigChange);
+    'change', handleConfigChange);
   document.getElementById('email-signInMethod-emailLink').addEventListener(
-      'change', handleConfigChange);
+    'change', handleConfigChange);
   // Check the selected email signInMethod mode.
   document.querySelector(
-      'input[name="emailSignInMethod"][value="' + getEmailSignInMethod() + '"]')
-      .checked = true;
+    'input[name="emailSignInMethod"][value="' + getEmailSignInMethod() + '"]')
+    .checked = true;
 };
 
 window.addEventListener('load', initApp);
