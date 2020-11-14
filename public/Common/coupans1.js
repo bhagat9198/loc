45df7ecb1327a62ac8ea94c05fc241972ae599ff
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-11-14 12:54:06 +0530

    minor coupan

--- public/Common/coupans1.js
+++ public/Common/coupans1.js
@@ -2,3 +2,11 @@ console.log("coupans1.js");
 
 const db = firebase.firestore();
 
+db.collection('coupans').onSnapshot(snapshots => {
+  let snapshotDocs = snapshots.docs;
+
+  snapshotDocs.map(doc => {
+    let docData = doc.data();
+    displayCard(docData);
+  })
+})
