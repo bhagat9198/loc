ec2348ae950d54e958731897fdcde3752daeff76
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-11-25 16:38:10 +0530

    functions

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,197 @@
 const functions = require("firebase-functions");
-const cryptoHmac = require("create-hmac");
 const Razorpay = require("razorpay");
 const admin = require("firebase-admin");
+const cryptoHmac = require("create-hmac");
 admin.initializeApp();
 
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const calBill = async(USER_ID, CHECKOUT_ID, coupan, shipeType, shipDate, shipTime, optional = null) => {
+
+  let userRef = await admin.firestore().collection("Customers").doc(USER_ID);
+  let userDetails;
+  await userRef
+    .get()
+    .then((doc) => {
+      let docData = doc.data();
+      userDetails = docData;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  let index = -1;
+  for (let o of userDetails.orders) {
+    index++;
+    if (Number(o.orderId) === Number(CHECKOUT_ID)) {
+      break;
+    }
+  }
+
+  let TOTAL_COST = 0;
+  // console.log('userDetails', userDetails);
+  // console.log('index', index);
+  for (let p of userDetails.orders[index].products) {
+    prodRef = admin.firestore().collection(p.cat).doc(p.prodId);
+    await prodRef.get().then((doc) => {
+      let docData = doc.data();
+      p.pdata = docData;
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
+
+  basicPrices = [];
+  for (let p of userDetails.orders[index].products) {
+    let totalProdPrice = 0;
+    let basicPrice = 0;
+    let heartPrice = 0;
+    let egglessPrice = 0;
+
+    if (p.cake) {
+      for (let w of p.pdata.weights) {
+        // console.log(w);
+        if (w.cakeWeight === p.cake.weight) {
+          basicPrice = w.weightPrice;
+          break;
+        } 
+      }
+
+      if (p.cake.eggless) {
+        egglessPrice = p.pdata.type.price ;
+      }
+
+      if (p.cake.heart) {
+        heartPrice = p.pdata.shapes[0].shapePrice;
+      }
+
+      totalProdPrice = (+basicPrice + +egglessPrice + +heartPrice) * (+p.qty);
+      totalProdPrice = Number(totalProdPrice.toFixed(2));
+      basicPrices.push(totalProdPrice);
+    } else {
+      totalProdPrice = (+p.pdata.sp) * (+p.qty);
+      totalProdPrice = Number(totalProdPrice.toFixed(2));
+      basicPrices.push(totalProdPrice);
+    }
+    TOTAL_COST = TOTAL_COST + totalProdPrice;
+    TOTAL_COST = Number(TOTAL_COST.toFixed(2));
+  }  
+  // console.log(TOTAL_COST);    
+  // console.log('basicPrices', basicPrices);
+  let bpArr = basicPrices.slice();
+  let disArr = [];
+  let totalSubTotal = 0;
+  basicPrices.map(p => {
+    totalSubTotal += +p;
+  })
+  if(coupan) {
+    let totalDis = 0;
+    await admin.firestore().collection('coupans').doc(coupan).get().then(doc => {
+      let docData = doc.data();
+        if(docData.minAmt <= totalSubTotal) {
+          basicPrices.map((el, i) => {
+            let d = el * (+docData.amount/100);
+            d = Number(d.toFixed(2));
+            disArr.push(d);
+            let eachDis =  el - (d);
+            eachDis = Number(eachDis.toFixed(2));
+            basicPrices[i] = eachDis;
+            totalDis += d;
+          });
+        }
+    })
+    TOTAL_COST = TOTAL_COST - totalDis;
+    TOTAL_COST = Number(TOTAL_COST.toFixed(2));
+    // console.log(TOTAL_COST);
+  }
+  // console.log('disArr', disArr);
+
+  let gstArr = [];
+  let counter = -1;
+  // console.log(basicPrices);
+  for (p of userDetails.orders[index].products) {
+    counter++;
+    let gstPrice = 0;
+    let gstPercent = 0;
+    gstPercent = +p.pdata.gst;
+    gstPrice = +basicPrices[counter] * (+gstPercent / 100);
+    gstArr.push(gstPrice);
+    gstPrice = Number(gstPrice.toFixed(2));
+    // console.log(TOTAL_COST);
+    TOTAL_COST = TOTAL_COST + gstPrice;
+  }
+  // console.log('gstArr', gstArr);
+
+  let addonCost = 0;
+  if (userDetails.orders[index].addons) {
+    if (userDetails.orders[index].addons.length > 0) {
+      for (addon of userDetails.orders[index].addons) {
+        await admin.firestore()
+          .collection("addons")
+          .doc(addon.id)
+          .get()
+          .then((addonDoc) => {
+            let addonData = addonDoc.data();
+            addonCost += Number(addonData.price) * Number(addon.qty);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    }
+  }
+  // console.log(addonCost);
+  // console.log(TOTAL_COST);
+  TOTAL_COST += addonCost;
+  // console.log(TOTAL_COST);
+  TOTAL_COST = Math.round(TOTAL_COST);
+
+  let shipTimeCost = 0;
+  let shipCat = '', timeDelivery = '', dateDelivery = '';
+  await admin.firestore().collection('miscellaneous').doc('shipTimePrice').get().then(shipDoc => {
+    let shipData = shipDoc.data();
+    // console.log(shipData);
+    shipData.shipTypes.map(ship => {
+      if(ship.type === shipeType) {
+        shipTimeCost = ship.charge;
+        shipCat = ship.type;
+        timeDelivery = shipTime;
+        dateDelivery = shipDate;
+      }
+    })
+  })
+
+  TOTAL_COST = TOTAL_COST + +shipTimeCost;
+
+  userDetails.orders[index].products.map(pp => {
+    delete pp.pdata;
+  })
+
+  if(optional.shipping) {
+    admin.firestore().collection('orders').add({
+      orderId: optional.orderId,
+      paymentId: optional.paymentId,
+      signature: optional.signature,
+      user: USER_ID,
+      shipping: optional.formData,
+      order: userDetails.orders[index],
+      basicPrice: bpArr,
+      disArr: disArr,
+      gstArr: gstArr,
+      addonCost: addonCost,
+      shipeType: shipCat,
+      shipDate: dateDelivery,
+      shipTime: timeDelivery
+    }).then(s => {
+      // console.log('saved', s);
+    }).catch(error => {
+      // console.log(error);
+    })
+  } else {
+    return TOTAL_COST;
+  }
+}
 
 exports.checkoutReq = functions.https.onCall(async (data, context) => {
   if (!context.auth) {
@@ -16,160 +201,49 @@ exports.checkoutReq = functions.https.onCall(async (data, context) => {
     );
   }
   console.log(data);
-  let USER_ID = data.userId;
-  let CHECKOUT_ID = data.order;
+  
+  const USER_ID = data.userId;
+  const CHECKOUT_ID = data.order;
+  const coupan = data.coupan;
+  const shipeType = data.type;
+  const shipDate = data.date;
+  const shipTime = data.time;
 
-  let userRef = admin.firestore().collection("Customers").doc(USER_ID);
-  let userDetails;
-  await userRef
-    .get()
-    .then((doc) => {
-      let docData = doc.data();
-      userDetails = docData;
-      return;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  let index = -1;
-  for (let o of userDetails.orders) {
-    index++;
-    if (Number(o.orderId) === Number(CHECKOUT_ID)) {
-      console.log(o);
-      break;
-    }
+  let optional = {
+    shipping: false
   }
+  let TOTAL_COST = await calBill(USER_ID, CHECKOUT_ID, coupan, shipeType, shipDate, shipTime, optional)
 
-  let TOTAL_COST = 0;
-  console.log('userDetails', userDetails);
-  console.log('index', index);
-  for (let p of userDetails.orders[index].products) {
-    prodRef = admin.firestore().collection(p.cat).doc(p.prodId);
-    await prodRef.get().then((doc) => {
-      let docData = doc.data();
-      p.pdata = docData;
-      return;
-    }).catch((error) => {
-      console.log(error);
-    })
-  }
-  // await Promise.all(userDetails.orders[index].products);
+  // const instance = new Razorpay({
+  //   key_id: "rzp_live_BfdC1FopDqRvQL",
+  //   key_secret: "AE2PN4j1ckML1UkpCRYggUxn",
+  // });
 
-  let basicPrices = [];
-
-  for (let p of userDetails.orders[index].products) {
-    let totalProdPrice = 0;
-    let basicPrice = 0;
-    let heartPrice = 0;
-    let egglessPrice = 0;
-
-    if (p.cake) {
-      for (let w of p.pdata.weights) {
-        if (w.cakeWeight === "half") {
-          basicPrice = w.weightPrice;
-          break;
-        } else if (w.cakeWeight === "one") {
-          basicPrice = w.weightPrice;
-          break;
-        } else if (w.cakeWeight === "oneHlaf") {
-          basicPrice = w.weightPrice;
-          break;
-        } else if (w.cakeWeight === "two") {
-          basicPrice = w.weightPrice;
-          break;
-        } else if (w.cakeWeight === "three") {
-          basicPrice = w.weightPrice;
-          break;
-        } else if (w.cakeWeight === "four") {
-          basicPrice = w.weightPrice;
-          break;
-        } else if (w.cakeWeight === "five") {
-          basicPrice = w.weightPrice;
-          break;
-        } else if (w.cakeWeight === "six") {
-          basicPrice = w.weightPrice;
-          break;
-        } else {
-          console.log("invalid");
-        }
-      }
-
-      if (p.cake.eggless) {
-        egglessPrice = p.pdata.type.price;
-      }
-
-      if (p.cake.heart) {
-        heartPrice = p.pdata.shapes[0].shapePrice;
-      }
-      // console.log(basicPrice, egglessPrice, heartPrice);
-      totalProdPrice = Math.round(Number(basicPrice) + Number(egglessPrice) + Number(heartPrice));
-      basicPrices.push(totalProdPrice);
-    } else {
-      totalProdPrice = Math.round(Number(p.pdata.sp));
-      basicPrices.push(totalProdPrice);
-    }
-    totalProdPrice = totalProdPrice * Number(p.qty);
-
-    TOTAL_COST = TOTAL_COST + totalProdPrice;
-  }
-
-  let counter = -1;
-  for (p of userDetails.orders[index].products) {
-    counter++;
-    let gstPrice = 0;
-    let gstPercent = 0;
-    gstPercent = Number(p.pdata.gst);
-    gstPrice = Math.round((basicPrices[counter] * Number(p.qty) * (Number(gstPercent) / 100)));
-    TOTAL_COST = TOTAL_COST + gstPrice;
-  }
-
-  let addonCost = 0;
-  if (userDetails.orders[index].addons) {
-    if (userDetails.orders[index].addons.length > 0) {
-      for (addon of userDetails.orders[index].addons) {
-        admin.firestore()
-          .collection("addons")
-          .doc(addon.id)
-          .get()
-          .then((addonDoc) => {
-            let addonData = addonDoc.data();
-            addonCost += Number(addonData.price()) * Number(addon.qty);
-            return;
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
-    }
-  }
-  TOTAL_COST += addonCost;
-
-  var instance = new Razorpay({
-    key_id: "rzp_live_BfdC1FopDqRvQL",
-    key_secret: "AE2PN4j1ckML1UkpCRYggUxn",
+  const instance = new Razorpay({
+    key_id: 'rzp_test_VkBZNRiEBUKNu5',
+    key_secret: 'T4Lx7KUbbfPaIHvRWQsxx4WL'
   });
 
   console.log(data);
 
-  let options = {
-    amount: 100,  
+  const options = {
+		amount: TOTAL_COST * 100,
 		currency: "INR",
-		receipt: "order_rcptid_11",
-		payment_capture: '0'
-  }
+		receipt: data.name,
+	}; 
 
   await instance.orders.create(options, (err, order) => {
     if (err) {
       console.log(err);
     }
-    console.log(options);
+    // console.log(options);
     options.orderId = order.id;
-    console.log(options);
+    // console.log(options);
   });
   return options;
 });
 
-exports.payemnetStatus = functions.https.onCall((data, context) => {
+exports.payemnetStatus = functions.https.onCall(async(data, context) => {
   if (!context.auth) {
     throw new functions.https.HttpsError(
       'unauthenticated',
@@ -178,19 +252,33 @@ exports.payemnetStatus = functions.https.onCall((data, context) => {
   }
   console.log(data);
   const razorpay_payment_id = data.razorpay_payment_id;
-  const razorpay_order_id = data.razorpay_order_id;
+	const razorpay_order_id = data.razorpay_order_id;
   const razorpay_signature = data.razorpay_signature;
-
-  const dataKey = razorpay_order_id + "|" + razorpay_payment_id;
-  const expectSig = cryptoHmac("sha256", "AE2PN4j1ckML1UkpCRYggUxn")
-    .update(dataKey.toString())
-    .digest("hex");
-  console.log(expectSig);
-  let status;
-  if (expectSig === razorpay_signature) {
-    status = true;
-  } else {
-    status = false;
+  const USER_ID = data.userId;
+  const CHECKOUT_ID = data.order;
+  const coupan = data.coupan;
+  const formData = data.formData;
+  const shipeType = data.type;
+  const shipDate = data.date;
+  const shipTime = data.time;
+   
+	const dataKey = razorpay_order_id + '|' + razorpay_payment_id; 
+  const expectSig = cryptoHmac('sha256', 'T4Lx7KUbbfPaIHvRWQsxx4WL').update(dataKey.toString()).digest('hex');
+  let status = false;
+	if(expectSig === razorpay_signature) {
+    // console.log('razorpay_signature', razorpay_signature);
+    status = "true";  
+    let shippingData = {
+      paymentId: razorpay_payment_id,
+      orderId: razorpay_order_id,
+      signature: razorpay_signature,
+      formData: formData,
+      shipping: true
+    }
+    await calBill(USER_ID, CHECKOUT_ID, coupan, shipeType, shipDate, shipTime, shippingData);
+  } else {  
+    console.log('razorpay_signature', razorpay_signature);  
+    status = "false";
   }
-  return res.status(200).json(status);
+  return status;
 });
