d0f0bd6dd3c06db9cd142c6090afda7f2da2d8de
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-12-03 21:26:25 +0530

    slider done

--- public/Home/Slider/slider1.js
+++ public/Home/Slider/slider1.js
@@ -7,6 +7,20 @@ const addSliderFormHTML = document.querySelector("#add-slider-form");
 const sliderFileHTML = document.querySelector("#slider-file");
 const sliderTbodyHTML = document.querySelector(".slider-tbody");
 let sliderImg;
+let allCatArr = [];
+
+const categoriesHTML = document.querySelector('#categories');
+
+db.collection('categories').onSnapshot(catSnaps => {
+  let catDocs = catSnaps.docs;
+  let options = `<option selected disabled value="">Select Category</option>`;
+  catDocs.map(c => {
+    let cData = c.data();
+    allCatArr.push({docId: c.id, catName: cData.name});
+    options += `<option value="${c.id}"> ${cData.name}</option>`;
+  })
+  categoriesHTML.innerHTML = options;
+})
 
 const extractImgURL = async (imgPath) => {
   // console.log(imgPath);
@@ -46,10 +60,20 @@ const displayRows = async (docs) => {
      
     }
     let imgUrl = await extractImgURL(`sliders/${docId}/${docData.img}`);
+    let catName = '';
+    for(let c of allCatArr) {
+      if(c.docId == docData.cat) {
+        catName = c.catName;
+        break;
+      }
+    }
     tRows += `
     <tr role="row" class="odd parent">
       <td tabindex="0">${docData.title}<br></td>
       <td><img src="${imgUrl}"></td>
+      <td>${catName}</td>
+      <td>${docData.midnight ? 'Selected' : 'Not Selected'}</td>
+      <td>${docData.daylight ? 'Selected' : 'Not Selected'}</td>
       <td>
       <div class="action-list">
         <select class="process  drop-success" style="display: block; " id="statusUpdate` + doc.id + `" onchange=statusUpdated("statusUpdate` + doc.id + `","` + doc.id + `")>
@@ -116,11 +140,29 @@ db.collection("sliders").onSnapshot((snapshot) => {
 const addSlider = (e) => {
   e.preventDefault();
   const title = addSliderFormHTML["slider-name"].value;
+  const catId = addSliderFormHTML['category-name'].value;
+  // const midnight = addSliderFormHTML['midnight'].value;
+  // const daylight = addSliderFormHTML['daylight'].value;
   const imgName = `${Math.random()}__${sliderImg.name}`;
+
+  console.log(catId, typeof(midnight), midnight, daylight);
+  let midnightState = false;
+  let daylightState = false;
+  if(addSliderFormHTML.querySelector('input[name=midnight]:checked')) {
+    midnightState = true;
+  }
+  if(addSliderFormHTML.querySelector('input[name=daylight]:checked')) {
+    daylightState = true;
+  }
+
+  console.log(midnightState);
 
   const wholdeSliderData = {
     title: title,
     img: imgName,
+    cat: catId,
+    midnight: midnightState,
+    daylight: daylightState
   };
 
   const addSliderReq = async (data) => {
@@ -147,6 +189,7 @@ const addSlider = (e) => {
         docData.imgUrl = imgUrl;
         docRef.update(docData);
       })
+      addSliderFormHTML.reset();
       sliderImg = null;
     })
     .catch((error) => {
@@ -159,3 +202,4 @@ sliderFileHTML.addEventListener("change", (e) => {
   sliderImg = e.target.files[0];
   // console.log(sliderImg);
 });
+
