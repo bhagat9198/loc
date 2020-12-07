eb77be7bab61d139d200ad2e6c490a217f7439df
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-12-07 18:29:14 +0530

    review

--- public/Customers/customersReview1.js
+++ public/Customers/customersReview1.js
@@ -17,36 +17,118 @@ db.collection("categories").onSnapshot((catSnaps) => {
   displayReviews();
 });
 
+const deleteReview = e => {
+  console.log(e.target.dataset);
+  let catid = e.target.dataset.catid;
+  let prodid = e.target.dataset.prodid;
+  let docid = e.target.dataset.docid;
+  let counter = e.target.dataset.counter;
+
+  console.log(catid, prodid, docid);
+  let delRef = db.collection('reviews').doc(catid).collection(prodid);
+  delRef.doc(docid).delete().then(async() => {
+    console.log('deleted');
+    document.querySelector(`#panel-${counter}`).remove();
+    let starsTotal = 0;
+    let numStars = 0;
+    await delRef.onSnapshot(psnaps => {
+      if(psnaps.exists) {
+        let psnapsDocs = psnaps.docs;
+        for(let pd of psnapsDocs) {
+          numStars++;
+          starsTotal = +pd.rating.split('__')[0];
+        }
+      }
+    })
+    console.log(starsTotal, numStars);
+    let avgStar = Math.round(starsTotal/numStars);
+
+    let pRef = await db.collection(catid).doc(prodid);
+    await pRef.get().then(psnap => {
+      if(psnap.exists) {
+        let pd = psnap.data();
+        pd.stars = avgStar;
+        pRef.update(pd);
+      }
+    })
+  }).catch(error => {
+    console.log(error);
+  })
+}
+
 
 let accordionExampleHTML = document.querySelector("#accordionExample");
 
-const displayReviews = async() => {
-  let allAccordCards = '';
+const displayReviews = async () => {
+  let allAccordCards = "";
   for (let c of allCategories) {
     let eachAccordCard = "";
     let eachAccordCardStart = "";
     let eachAccordCardEnd = "";
-    let prouctsReviewed = '';
+    let prouctsReviewed = "";
     let singleCatReviwsRef = await db.collection("reviews").doc(c.catId);
-    await singleCatReviwsRef.get().then(async(snapshot) => {
+    await singleCatReviwsRef.get().then(async (snapshot) => {
       let snapshotData = snapshot.data();
+      console.log(snapshotData);
       if (snapshotData) {
         if (snapshotData.collectionIds) {
-          console.log(snapshotData);
+          // console.log(snapshotData);
+
           let allCollectionIds = snapshotData.collectionIds;
           for (let collectionId of allCollectionIds) {
-            let eachProdStart = '';
-            let eachProdEnd = '';
-            let eachProd = '';
-            let prodName = '';
+            console.log(collectionId);
+            let eachProdStart = "";
+            let eachProdEnd = "";
+            let eachProd = "";
+            let prodName = "";
+            let card = "";
+
             await singleCatReviwsRef
               .collection(collectionId)
               .get()
               .then((prodReviewSnaps) => {
                 let prodReviewSnapsDocs = prodReviewSnaps.docs;
+                let counter = 0;
                 for (let rev of prodReviewSnapsDocs) {
+                  counter++;
                   let revData = rev.data();
-                  console.log(revData);
+                  // console.log(revData);
+                  prodName = revData.prodName;
+                  card += `
+                  <div class="panel panel-info" id="panel-${counter}">
+                    <div class="panel-heading" style="display: flex;">
+                      <h5>${revData.userName}</h5>
+                      <div class="userrating">
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star"></span>
+                        <span class="fa fa-star"></span>
+                      </div>
+                    </div>
+                    <div class="panel-body" style="display: flex;">
+                      <div style="width: 30%;">
+                        <img src="${revData.pimg}" width="150px" alt="" srcset="">
+                      </div>
+                      <div>
+                        <small>User Id - ${revData.user}</small>
+                        <h5> <strong>Product Sno - ${revData.sno}</strong> </h5>
+                        <button class="btn btn-primary reviewBtn" type="button" data-toggle="collapse"
+                          data-target="#collapseExample-${revData.user}-${counter}" aria-expanded="false" aria-controls="collapseExample-${revData.user}-${counter}">
+                          Read Review
+                        </button>
+                        <button class="btn btn-primary deleteBtn" type="button" data-counter="${counter}" data-catId="${c.catId}" data-prodId="${collectionId}" data-docId="${rev.id}"  onclick="deleteReview(event)">
+                          Delete Review
+                        </button>
+                        <div class="collapse " id="collapseExample-${revData.user}-${counter}">
+                          <div class="card card-body reviewbody">
+                            ${revData.msg}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  `;
                 }
               });
 
@@ -55,7 +137,7 @@ const displayReviews = async() => {
               <div class="card-header" id="heading-${c.collectionId}">
                 <h2 class="mb-0">
                   <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse-${c.collectionId}" aria-expanded="true" aria-controls="collapseOne">
-                    ${c.catName}
+                    ${prodName}
                   </button>
                 </h2>
               </div>
@@ -64,17 +146,19 @@ const displayReviews = async() => {
             `;
 
             eachProdEnd = `
+                  </div>
                 </div>
-              </div>
-            </div>;
-          `;
+              </div>;
+            `;
 
-          eachProd = eachProdStart + eachProdEnd;
+            eachProd = eachProdStart + card + eachProdEnd;
+            // console.log(eachProd);
+            prouctsReviewed += eachProd;
           }
         }
       } else {
         // no reviews
-        console.log(snapshotData);
+        // console.log(snapshotData);
         prouctsReviewed = `
         <div style="color: red;">
           <h4>No Products Reviwed for this category</h4>
@@ -103,7 +187,7 @@ const displayReviews = async() => {
     </div>;
     `;
     eachAccordCard = eachAccordCardStart + prouctsReviewed + eachAccordCardEnd;
-    console.log(eachAccordCard);
+    // console.log(eachAccordCard);
     allAccordCards += eachAccordCard;
   }
   accordionExampleHTML.innerHTML = allAccordCards;
