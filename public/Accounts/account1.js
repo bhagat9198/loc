8fcecfc03aad11b68b6481e5f60b8ec1c174ff73
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2021-01-20 00:04:16 +0530

    accounts consolelogs

--- public/Accounts/account1.js
+++ public/Accounts/account1.js
@@ -624,9 +624,9 @@ const displayOrders = () => {
 };
 
 const sortOrders = async() => {
-  console.log(ALL_ORDERS);
+  // console.log(ALL_ORDERS);
   ALL_ORDERS.sort((a,b) => {
-    console.log(a);
+    // console.log(a);
     let d1 = a.timeStamp;
     let d2 = b.timeStamp;
     // console.log(d1, d2);
@@ -640,7 +640,7 @@ db.collection("orders").get().then(async(snaps) => {
 
   for (let doc of sanpsDocs) {
     let data = doc.data();
-    console.log(data);
+    // console.log(data);
     ALL_ORDERS.push(data);
   }
   
